5b90a4515cf4a636010a92fe6b1f6f3b93995516
Shai Gerbi <ShaiGerbi@gmail.com>
2019-08-21 09:58:41 +0300

    Create create function

--- js/source.js
+++ js/source.js
@@ -2,16 +2,22 @@ let list = document.getElementById('list');
 let text = document.getElementById('text');
 let add  = document.getElementById('add');
 
-add.addEventListener('click', () =>
+add.addEventListener('click', (event) =>
 {
+    event.preventDefault();
+
     if(!text.value)
     {
         return alert('Fill the task');
     }
 
-    let newItem = document.createElement('li');
-    newItem.innerHTML = text.value;
-    list.append(newItem);
+    create(text.value);
 
     text.value = null;
 });
+
+function create(text)
+{
+    let newItem = `<li>${text}</li>`;
+    list.insertAdjacentHTML('beforeend', newItem);
+}
